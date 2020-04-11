2f6b0eeb7196aae4eeb5b8a7c8c6aea7dc01c616
Aquarius223 <2777968262@qq.com>
2020-04-11 14:39:55 +0800

    clear build_compiler.sh

--- build_kernel2.sh
+++ build_kernel2.sh
@@ -2,10 +2,6 @@
 export CROSS_COMPILE=/home/stic/桌面/msm8974/arm-eabi-4.9.4/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
-export KBUILD_BUILD_HOST="Xiaomi MIX 2S"
-export KBUILD_BUILD_USER=root
 mkdir out
-make O=out clean
-make O=out mrproper
 make O=out stic-cancro_defconfig
-make CONFIG_NO_ERROR_ON_MISMATCH=y O=out -j16
+make O=out -j16
